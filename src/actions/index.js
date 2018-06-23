fd6188edb4ffb08d6332b0576927007cafab8f39
burovg <burovg@gmail.com>
2018-06-23 23:53:57 +0300

    3

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,15 @@
+import axios from 'axios';
+
 export const FETCH_USERS = 'fetch_users';
+
+export function fetchUsers(){
+    const req = axios.get("http://jsonplaceholder.typicode.com/users");
+
+    return (dispatch) => {
+        req.then(({data}) => 
+            {
+                dispatch({type : FETCH_USERS,payload : data});
+            }
+        );
+    };
+}
--- src/components/users.js
+++ src/components/users.js
@@ -1,14 +1,22 @@
 import React,{Component} from 'react';
 import {connect} from 'react-redux';
 
+import * as actions from '../actions';
+
 class Users extends Component{
+
+    componentWillMount(){
+        this.props.fetchUsers();
+    }
+
     render(){
         return(
             <div>
                 Users
+                {console.log(this.props)}
             </div>
         );
     }
 }
 
-export default connect(null)(Users);;
+export default connect((state => state),actions)(Users);
